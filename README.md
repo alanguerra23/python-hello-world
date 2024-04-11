e0a16db4cd06d57f6eac683914482e9da2ae233d
Alan Guerra <164754267+alanguerra23@users.noreply.github.com>
2024-04-11 08:14:15 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello World
+
+Criando o primeiro hello world usando a linguagem de programação python.
---
+++ main.py
@@ -0,0 +1,13 @@
+def return_message():
+    message = 'Hello World'
+
+    return message
+
+def main():
+    message = return_message()
+
+    print(message)
+
+main()
+
+exit()
